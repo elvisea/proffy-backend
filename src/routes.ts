43c7f14531ad5adfeb2d507e913a56c98b95d061
Elvis <elvis.e.amancio@gmail.com>
2021-01-12 21:05:16 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
+
+const routes = express.Router()
+
+const classesController = new ClassesController();
+const connectionsControllers = new ConnectionsController();
+
+routes.post('/classes', classesController.create);
+routes.get('/classes', classesController.index);
+
+routes.post('/connections', connectionsControllers.create);
+routes.get('/connections', connectionsControllers.index);
+
+export default routes;
